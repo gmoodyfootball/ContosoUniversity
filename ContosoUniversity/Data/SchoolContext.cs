e5eafec46f6f2dd28a236d900842a4ac1a0877ff
gmoodyfootball <gmoodyfootball@gmail.com>
2018-03-29 09:30:49 -0600

    Halfway through page 3 of tutorial. I'm at "add a search box to the student index view"

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+        //An entity set typically corresponds to a database table
+        //An entity corresponds to a row in the table
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        //This is to prevent the Database from naming the columns incorrectly (With plural names, instead of singular)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
