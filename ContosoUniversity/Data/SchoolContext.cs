da639d2c0bafc66f9e5fc558a3b39e902f00b7b6
gmoodyfootball <gmoodyfootball@gmail.com>
2018-03-30 23:17:33 -0600

    Step 9 - Inheritance done

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -9,8 +9,6 @@ namespace ContosoUniversity.Data
         {
         }
 
-        //An entity set typically corresponds to a database table
-        //An entity corresponds to a row in the table
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
@@ -18,8 +16,8 @@ namespace ContosoUniversity.Data
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
         public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
 
-        //This is to prevent the Database from naming the columns incorrectly (With plural names, instead of singular)
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
@@ -29,6 +27,7 @@ namespace ContosoUniversity.Data
             modelBuilder.Entity<Instructor>().ToTable("Instructor");
             modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
             modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+            modelBuilder.Entity<Person>().ToTable("Person");
 
             modelBuilder.Entity<CourseAssignment>()
                 .HasKey(c => new { c.CourseID, c.InstructorID });
